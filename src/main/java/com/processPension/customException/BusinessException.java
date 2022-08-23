bc1b87f0bf5778cf298b8c2d3d6931ffd8bcec3c
MANISH <ms7644030@gmail.com>
2022-08-23 08:41:42 +0530

    first commit

---
+++ src/main/java/com/processPension/customException/BusinessException.java
@@ -0,0 +1,21 @@
+package com.processPension.customException;
+
+import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Component
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BusinessException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	private int errorcode;
+	private String errorMessage;
+
+}
